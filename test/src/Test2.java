9c29aba184814e6770457d0894c2e8c094a4d6e5
olinaf11 <fanilokevin@icloud.com>
2023-06-06 16:12:12 +0300

    add class singleton

--- test/src/Test2.java
+++ test/src/Test2.java
@@ -1,10 +1,30 @@
 package model;
 
-import etu2028.framework.annotation.Url;
+import etu2028.framework.ModelView;
+import etu2028.framework.annotation.*;
 
+@Scope(value = "singleton")
 public class Test2 {
+    String ok;
+    Integer count = Integer.valueOf(0);
+    public Integer getCount() {
+        return count;
+    }
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getOk() {
+        return ok;
+    }
+    public void setOk(String ok) {
+        this.ok = ok;
+    }
+
     @Url(name = "test2-insert")
-    public void insert(){
-        return;
+    public ModelView insert(){
+        ModelView modelView = new ModelView("singleton.jsp");
+        modelView.addItem("test", this);
+        return modelView;
     }
 }
